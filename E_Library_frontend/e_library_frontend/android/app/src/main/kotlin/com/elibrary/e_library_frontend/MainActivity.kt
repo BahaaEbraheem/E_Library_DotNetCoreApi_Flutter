0a52e67822bc8f9e7ab993efcdcdad7905e510cf
bahaa ebraheem <Bahaa.ebraheem812@outlook.com>
2025-05-28 16:42:28 +0300

    commit add flutter app

---
+++ E_Library_frontend/e_library_frontend/android/app/src/main/kotlin/com/elibrary/e_library_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.elibrary.e_library_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- backend/Services/IPasswordService.cs
+++
@@ -1,7 +0,0 @@
-namespace E_Library.API.Services;
-
-public interface IPasswordService
-{
-    string HashPassword(string password);
-    bool VerifyPassword(string password, string hash);
-}
